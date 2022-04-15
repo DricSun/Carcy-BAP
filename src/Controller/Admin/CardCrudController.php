7b1b7c1161c4d63e963e6e0b156ef2f246a6df91
Cedric <cedric.jamme@edu.devinci.fr>
2022-04-15 23:38:27 +0200

    add faq style add style bootstrap with cdn without js :lipstick:

--- src/Controller/Admin/CardCrudController.php
+++ src/Controller/Admin/CardCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Card;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -19,9 +20,18 @@ class CardCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->setDisabled(),
+            TextField::new('name'),
+            TextField::new('first_name'),
+            ImageField::new('image_user')
+                ->setUploadDir('public/uploads/image-user')
+                ->setBasePath('uploads/images-user')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setFormTypeOptions([
+                    'attr' => [
+                        'accept' => 'image/jpeg, image/png'
+                    ]
+                ]),
         ];
     }
 
