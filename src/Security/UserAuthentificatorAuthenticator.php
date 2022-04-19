81a1e4af9aaaf8b11c6bba49ad513a37b95ed7a9
Mouchon <william.mouchon@edu.devinci.fr>
2022-04-19 11:50:24 +0200

    recup

--- src/Security/UserAuthentificatorAuthenticator.php
+++ src/Security/UserAuthentificatorAuthenticator.php
@@ -51,7 +51,7 @@ class UserAuthentificatorAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('app_faq'));
     }
 
     protected function getLoginUrl(Request $request): string
